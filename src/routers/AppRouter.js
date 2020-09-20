13b6ad24ff5b020674167dc299caeb86f59daf39
Adrian1426 <adrianhh1426@gmail.com>
2020-09-19 20:09:59 -0500

    componente de ruta privada - función que revisa si está auntenticado

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {
     BrowserRouter as Router,
     Switch,
     Route
-  } from 'react-router-dom';
+} from 'react-router-dom';
+import PrivateRoute from '../routers/PrivateRoute';
+import { AuthContext } from '../auth/AuthContext';
 
 
 import { LoginScreen } from '../components/login/LoginScreen';
@@ -11,13 +13,16 @@ import { DashboardRoutes } from './DashboardRoutes';
 
 
 export const AppRouter = () => {
+
+    const { userState: { logged } } = useContext(AuthContext);
+
     return (
         <Router>
             <div>
-                <Switch> 
-                    <Route exact path="/login" component={ LoginScreen } />
-                    
-                    <Route path="/" component={ DashboardRoutes } />
+                <Switch>
+                    <Route exact path="/login" component={LoginScreen} />
+
+                    <PrivateRoute path="/" isAutenticated={logged} component={DashboardRoutes} />
                 </Switch>
             </div>
         </Router>
---
+++ src/routers/PrivateRoute.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const PrivateRoute = ({ isAutenticated, component: Component, ...resto }) => {
+  return (
+    <Route
+      {...resto}
+      component={props => (
+        isAutenticated ? <Component {...props} /> : <Redirect to="/login" />
+      )}
+    />
+  );
+};
+
+PrivateRoute.propTypes = {
+  isAutenticated: PropTypes.bool.isRequired,
+  component: PropTypes.func.isRequired
+}
+
+export default PrivateRoute;
